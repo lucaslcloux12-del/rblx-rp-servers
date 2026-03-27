6c0eceacd44d9c5341fda9c23a79d3fc82f27c8f
lucaslcloux12-del <lucaslcloux12@gmail.com>
2026-03-27 03:10:17 -0300

    Refactor Firebase imports and GoogleAuthProvider usage

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtOTG-LUabRBohHtXy2veHFHQfsP0GINY",
@@ -13,4 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const googleProvider = new (require("firebase/auth").GoogleAuthProvider)();
+export const googleProvider = new GoogleAuthProvider();
