1ffd591f1e95174779710b5c2d89b92f3184b53c
lucaslcloux12-del <lucaslcloux12@gmail.com>
2026-03-29 17:35:03 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,24 +5,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDtOTG-LUabRBohHtXy2veHFHQfsP0GINY",
   authDomain: "rblx-rp-servers.firebaseapp.com",
-  projectId: "rblx-rp-servers",import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDtOTG-LUabRBohHtXy2veHFHQfsP0GINY",
-  authDomain: "rblx-rp-servers.firebaseapp.com",
   projectId: "rblx-rp-servers",
-  storageBucket: "rblx-rp-servers.firebasestorage.app",
-  messagingSenderId: "42920368528",
-  appId: "1:42920368528:web:b112faf341adb670273b15",
-  measurementId: "G-YCSM0BMZ0N"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
   storageBucket: "rblx-rp-servers.firebasestorage.app",
   messagingSenderId: "42920368528",
   appId: "1:42920368528:web:b112faf341adb670273b15",
